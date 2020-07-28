05a51d503b471d1febd998543e4855dec40e1b08
Leonardo Zangrando <lzangrando@gmail.com>
2020-07-28 13:53:33 +0100

    added prev link in blog post

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -23,6 +23,18 @@ export default function Blog(props) {
     }
   }
 
+  function getPrevSlug(slug) {
+    const allSlugs = allBlogData.map(blog => {
+      return blog.node.fields.slug
+    })
+    const prevSlug = allSlugs[allSlugs.indexOf(slug) - 1]
+    if(prevSlug !== undefined && prevSlug !== '') {
+      return prevSlug
+    } else {
+      return allSlugs[allSlugs.length()]
+    }
+  }
+
   return (
     <Layout>
       <article className={blogTemplateStyles.blog}>
@@ -41,6 +53,11 @@ export default function Blog(props) {
           dangerouslySetInnerHTML={{ __html: data.html }}
         ></div>
         <div className={blogTemplateStyles.blog__footer}>
+          <Link to={`/blog/${prevSlug}`} className={blogTemplateStyles.footer__next}>
+            <svg xmlns="http://www.w3.org/2000/svg"  version="1.1" x="0px" y="0px" viewBox="0 0 26 26" enableBackground="new 0 0 26 26" >
+              <path d="M23.021,12.294l-8.714-8.715l-1.414,1.414l7.007,7.008H2.687v2h17.213l-7.007,7.006l1.414,1.414l8.714-8.713  C23.411,13.317,23.411,12.685,23.021,12.294z"/>
+            </svg>
+          </Link>
           <h2>
             Written By: {data.frontmatter.author}
           </h2>
